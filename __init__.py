df55da7fb69e04a0154970680447fb2c28098a7e
Guillaume Gay <guillaume@mitotic-machine.org>
2014-09-19 12:16:00 +0200

    allowing to load only a chunk of the graph

--- __init__.py
+++ __init__.py
@@ -1,3 +1,1 @@
-from .hdfgraph import (graph_to_dataframes, graph_from_dataframes,
-                       frames_to_hdf, graph_to_hdf,
-                       frames_from_hdf, graph_from_hdf)
+from .hdfgraph import *
--- hdfgraph.py
+++ hdfgraph.py
@@ -11,8 +11,13 @@ from __future__ import print_function
 
 import os
 import numpy as np
-import graph_tool.all as gt
 import pandas as pd
+
+try:
+    from graph_tool import Graph
+except ImportError:
+    print('''graph-tool is not available, you won't be able to load data as a graph
+          You can still read the tables though''')
 
 
 TYPES_WHITELIST = ['bool', 'uint8_t', 'double', 'float',
@@ -34,13 +39,13 @@ ALIASES = {'float64':'double', 'int8':'bool', 'int32':'int'}
 
 def graph_to_dataframes(graph, stamp=None):
     '''Function packs the vertex and edge propery maps of
-    an **unfiltered** gt.Graph object in two dataframes.
+    an **unfiltered** Graph object in two dataframes.
 
     parameters:
     ===========
 
-    graph: a `gt.Graph` object whose 'internal' property maps will be output.
-    
+    graph: a `Graph` object whose 'internal' property maps will be output.
+
     stamp: an integer or a time stamp, default `None`. If `stamp` is
         not None, it will be used as a supplementary index, this is
         usefull to store a dynmical graph history.
@@ -52,7 +57,7 @@ def graph_to_dataframes(graph, stamp=None):
         PropertyMaps as columns.  If `stamp` is `None`, the DataFrame
         index corresponds to the graph's vertices indices, else it
         is a pd.MultiIndex with (stamp, index) pairs.
-    
+
     edge_df : a pd.DataFrame containing the graph's edge PropertyMaps
         as columns. If `stamp` is `None`, the DataFrame index is a
         pd.MultiIndex with `(source, target)` pairs, where `source`
@@ -98,22 +103,22 @@ def graph_to_dataframes(graph, stamp=None):
     valid = _get_valid_mask(graph)
     graph.set_edge_filter(valid)
     edge_df = pd.DataFrame({key: np.array(prop.fa, dtype=prop.fa.dtype)
-                            for key, prop in graph.edge_properties.items() 
+                            for key, prop in graph.edge_properties.items()
                             if prop.value_type() in TYPES_WHITELIST},
                            index=edge_index)
     graph.set_edge_filter(None)
     return vertex_df, edge_df
 
-def frames_to_hdf(vertex_df, edge_df, fname, reset=False, **kwargs):
+def frames_to_hdf(vertex_df, edge_df, fname, reset=False, vert_kwargs={}, edge_kwargs={}):
     '''
     Records the two  DataFrame in the hdf file filename
     '''
     with pd.get_store(fname) as store:
         if not len(store.keys()):
             store.put('edges', edge_df,
-                      format='table', **kwargs)
+                      format='table', **vert_kwargs)
             store.put('vertices', vertex_df,
-                      format='table', **kwargs) 
+                      format='table', **edge_kwargs)
         ## FIXME: should only remove the matching index
         elif reset:
             try:
@@ -125,18 +130,18 @@ def frames_to_hdf(vertex_df, edge_df, fname, reset=False, **kwargs):
             except KeyError:
                 pass
             store.put('vertices', vertex_df,
-                      format='table', **kwargs) 
+                      format='table', **vert_kwargs)
             store.put('edges', edge_df,
-                      format='table', **kwargs)
+                      format='table', **edge_kwargs)
         else:
             store.append('vertices', vertex_df,
-                         format='table', **kwargs) 
+                         format='table', **vert_kwargs)
             store.append('edges', edge_df,
-                         format='table', **kwargs)
-           
+                         format='table', **edge_kwargs)
+
 def graph_to_hdf(graph, fname, stamp=None, reset=False, **kwargs):
     '''
-    
+
     '''
     vertex_df, edge_df = graph_to_dataframes(graph, stamp=stamp)
     frames_to_hdf(vertex_df, edge_df, fname, reset=reset, **kwargs)
@@ -147,7 +152,7 @@ def graph_from_dataframes(vertex_df, edge_df):
 
     Paramters:
     ==========
-    verex_df: a DataFrame with an index named 'vertex_index'    
+    verex_df: a DataFrame with an index named 'vertex_index'
     edge_df: a DataFrame with a multi-index named ('source', 'target')
 
     Returns:
@@ -156,8 +161,8 @@ def graph_from_dataframes(vertex_df, edge_df):
         from the columns of the input DataFrames
     '''
 
-    graph = gt.Graph(directed=True)
-        
+    graph = Graph(directed=True)
+
     vertex_index = vertex_df.index.get_level_values(level='vertex_index')
     vertices = graph.add_vertex(n=vertex_index.shape[0])
     for col in vertex_df.columns:
@@ -168,6 +173,7 @@ def graph_from_dataframes(vertex_df, edge_df):
 
     src = edge_df.index.names.index('source')
     trgt = edge_df.index.names.index('target')
+    ### TODO: use the list edge creation
     for tup in edge_df.index:
         source, target = tup[src], tup[trgt]
         edge = graph.add_edge(source, target)
@@ -179,15 +185,87 @@ def graph_from_dataframes(vertex_df, edge_df):
         graph.edge_properties[col] = prop
     return graph
 
-def frames_from_hdf(fname, stamp=None, vertex_kwargs={}, edge_kwargs={}):
-    
+def table_from_hdf(fname, table, stamp=None, **kwargs):
     with pd.get_store(fname) as store:
-        if stamp is not None:
+        if stamp == -1:
+
+            edge_df = store.select(table,
+                                   **kwargs)
+            stamps = edge_df.index.get_level_values('stamp').unique()
+            last = stamps.max()
+            edge_df = edge_df.xs(last, level='stamp')
+
+        elif stamp is not None:
+            edge_df = store.select(table,
+                                   where="'stamp'={}".format(stamp),
+                                   **kwargs)
+        else:
+            edge_df = store.select(table,
+                                   **kwargs)
+    return edge_df
+
+def table_slice(fname, table, bounds):
+    '''
+    Small utility function to get the data only between bounds
+
+    Parameters:
+    -----------
+
+    fname: str, path to the HDF file
+    bounds: dict
+        key, value pairs of the form 'indexable':(min_value, max_value)
+        if one of those values is None, it will not be taken into account
+        (ie take the absolute min or max) as for range(), start is included and stop excluded)
+    Each of the bounds will be joined to the 'where' keywarg (by logical and)
+
+    Note:
+    -----
+    This will only work from indexable columns, aka data columns, which must be set
+    explicetely when recording the table. See the corresponding error when you don't
+    use one of those. See
+    http://pandas.pydata.org/pandas-docs/stable/io.html#query-via-data-columns
+
+    '''
+    conditions = []
+    for column, (min_value, max_value) in bounds.items():
+        start = "{} >= {}".format(column, min_value) if min_value is not None else ""
+        stop = "{} < {}".format(column, max_value) if max_value is not None else ""
+        conditions.extend([start, stop])
+    where = " & ".join([cnd for cnd in conditions if len(cnd)])
+    with pd.get_store(fname) as store:
+        if len(where):
+            return store.select(table, where=where)
+        else:
+            return store.select(table)
+
+def edges_time_slice(fname, start_stamp, stop_stamp):
+    return table_slice(fname, 'edges', {'stamp':(start_stamp, stop_stamp)})
+
+def vertices_time_slice(fname, start_stamp, stop_stamp):
+    return table_slice(fname, 'vertices', {'stamp':(start_stamp, stop_stamp)})
+
+
+
+def frames_from_hdf(fname, stamp=None,
+                    vertex_kwargs={}, edge_kwargs={}):
+
+    with pd.get_store(fname) as store:
+        if stamp == -1:
             vertex_df = store.select('vertices',
-                                     where="'stamp'=stamp",
+                                     #where="'stamp'=stamp",
+                                     **vertex_kwargs)
+            stamps = vertex_df.index.get_level_values('stamp').unique()
+            last = stamps.max()
+            vertex_df = vertex_df.xs(last, level='stamp')
+            edge_df = store.select('edges',
+                                   where="'stamp'={}".format(last),
+                                   **edge_kwargs)
+        elif stamp is not None:
+            vertex_df = store.select('vertices',
+                                     where="'stamp'={}".format(stamp),
                                      **vertex_kwargs)
             edge_df = store.select('edges',
-                                   where="'stamp'=stamp",
+                                   where="'stamp'={}".format(stamp),
                                    **edge_kwargs)
         else:
             vertex_df = store.select('vertices',
@@ -208,3 +286,31 @@ def _get_valid_mask(graph):
     for edge in graph.edges():
         valid[edge] = 1
     return valid
+
+
+
+
+def slice_data(vertices_df, edges_df, v_bounds={}):
+
+    for col, (v_min, v_max) in v_bounds.items():
+        vertices_df = vertices_df[vertices_df[col] >= v_min]
+        vertices_df = vertices_df[vertices_df[col] <= v_max]
+    vertex_index = set(vertices_df.index.get_level_values('vertex_index'))
+
+    src_index = set(edges_df.index.get_level_values('source'))
+    trgt_index = set(edges_df.index.get_level_values('target'))
+    srcs = src_index.intersection(vertex_index)
+    trgts = trgt_index.intersection(vertex_index)
+    edges_df['keep'] = True
+    edges_df = edges_df.groupby(level='source').apply(select_idx_, 'source', srcs)
+    edges_df = edges_df[edges_df['keep']]
+    edges_df = edges_df.groupby(level='target').apply(select_idx_, 'target', trgts)
+    edges_df = edges_df[edges_df['keep']]
+    return vertices_df, edges_df
+
+
+def select_idx_(df, idx_name, valids):
+    idx = df.index.get_level_values(idx_name)[0]
+    if idx not in valids:
+        df['keep'] = False
+    return df
